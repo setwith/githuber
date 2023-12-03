7f7026e4905a043ee092270ab954449f8d7a5994
Nazar Volynets <93925872+setwith@users.noreply.github.com>
2023-12-03 20:06:04 +0200

    update controller

--- app/controllers/github_info_controller.rb
+++ app/controllers/github_info_controller.rb
@@ -1,2 +1,23 @@
 class GithubInfoController < ApplicationController
+   def index
+  end
+
+  def get_info
+    login = params[:github_login]
+    query = <<~GRAPHQL
+      query {
+        getUserInfo(login: "#{login}") {
+          name
+          repos {
+            name
+          }
+        }
+      }
+    GRAPHQL
+
+    result = GithuberSchema.execute(query)
+    @user_info = result['data']['getUserInfo']
+    # debugger
+    render :index
+  end
 end
