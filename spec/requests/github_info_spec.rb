b3eac76396ead214ed366123e348009f3f95e74b
Nazar Volynets <93925872+setwith@users.noreply.github.com>
2023-12-04 17:07:58 +0200

    run rubocop

--- spec/requests/github_info_spec.rb
+++ spec/requests/github_info_spec.rb
@@ -1,31 +1,33 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "GithubInfos", type: :request do
-  describe "GET /index" do
-    it "renders the index template" do
+RSpec.describe 'GithubInfos' do
+  describe 'GET /index' do
+    it 'renders the index template' do
       get '/'
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
       expect(response).to render_template(:index)
     end
 
-    it "renders user information when valid login" do
+    it 'renders user information when valid login' do
       get '/github_info/get_info', params: { github_login: 'setwith' }
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
       expect(response).to render_template(:index)
       expect(response.body).to include('User Info:')
       expect(response.body).to include('Repositories:')
     end
 
-    it "renders an error message when invalid login" do
+    it 'renders an error message when invalid login' do
       get '/github_info/get_info', params: { github_login: 'invalid_login' }
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
       expect(response).to render_template(:index)
       expect(response.body).to include('Name not found for the given GitHub account')
     end
 
-    it "renders a message when no login" do
+    it 'renders a message when no login' do
       get '/github_info/get_info'
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
       expect(assigns(:user_info)['name']).to be_nil
       expect(assigns(:user_info)['repos']).to be_nil
       expect(response).to render_template(:index)
